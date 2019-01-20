bc8f185438a46c7690a7f6e5d0dcc539b127d593
sehmim <sehmim2010@gmail.com>
2019-01-20 11:41:29 -0500

    about to delete all the flexbox

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -65,7 +65,7 @@ class LandingPage extends Component {
         </div>
         
       <div className="picture" >
-          <img src="https://upload.wikimedia.org/wikipedia/en/4/4d/Shrek_%28character%29.png" className="picture-img" ></img>
+          {/* <img src="https://i.imgur.com/nJjdX1F.jpg" className="picture-img" ></img> */}
       </div>  
 
       </div>
